b6b070695da5a50254473fc550b02253bdb55489
Jakwan Shahir Siddique <jssiddiques@gmail.com>
2022-08-22 21:46:50 +0530

    Commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { BrowserRouter,Route,Switch } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header/Header';
+import SimpleBottomNavigation from './components/MainNav';
+import Trending from './Pages/Trending/Trending';
+import Series from './Pages/Series/Series';
+import Movies from './Pages/Movies/Movies';
+import Search from './Pages/Search/Search';
+import {Container} from '@material-ui/core';
+
+function App() {
+  return (
+    <BrowserRouter >
+    <Header />
+    <div className="app">
+    <Container>
+          <Switch>
+            <Route path="/" component={Trending} exact />
+            <Route path="/movies" component={Movies} />
+            <Route path="/series" component={Series} />
+            <Route path="/search" component={Search} />
+          </Switch>
+        </Container>
+    </div>
+
+
+      <SimpleBottomNavigation/>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,8 @@
+import './Header.css';
+const Header =()=>{
+    return(
+        <span onClick={()=>window.scroll(0,0)} className="header"> 🎞Movie Info🎞 </span>
+    )
+}
+
+export default Header;
